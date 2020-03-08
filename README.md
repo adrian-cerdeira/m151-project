6888bf513b69138aaf5c802726e4407c55fd5bd4
Adrian Cerdeira <acwebdesign01@gmail.com>
2020-03-08 14:54:26 +0100

    Items Entity erstellt

--- README.md
+++ README.md
@@ -5,4 +5,15 @@
 - PHP MVC Framework [Symfony](https://symfony.com/)
 - CSS-Framework mit [Bulma](https://bulma.io/)
 - Tooling mit Dependency-Manager [Composer](https://getcomposer.org/)
+- Datenbankmanager mit [Doctrine](https://www.doctrine-project.org/)
 - Templates mit [Twig](https://twig.symfony.com/doc/3.x/)
+
+## Update Database
+
+### Create Migration
+
+    php bin/console doctrine:migrations:diff
+
+### Push Migrations
+
+    php bin/console doctrine:migrations:migrate
---
+++ src/Entity/Items.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ItemsRepository")
+ */
+class Items
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=100)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $amount;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getAmount(): ?string
+    {
+        return $this->amount;
+    }
+
+    public function setAmount(string $amount): self
+    {
+        $this->amount = $amount;
+
+        return $this;
+    }
+}
