f0b284b2f897de8aa0daafae7df1803016ed36db
Adrian Cerdeira <acwebdesign01@gmail.com>
2020-03-08 14:07:28 +0100

    Testing Hallo World with route and method get

--- src/Controller/ItemController.php
+++ src/Controller/ItemController.php
@@ -1,9 +1,19 @@
 <?php
-    namespace App\Controller;
-    use Symfony\Component\HttpFoundation\Response;
 
-    class ItemController {
-        public function index() {
-            return new Response('<html><body>HALLO</body></html>');
-        }
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+
+class ItemController
+{
+    /**
+     * @Route("/")
+     * @Method({"GET"})
+     */
+    public function index()
+    {
+        return new Response('<html><body>HALLO</body></html>');
     }
+}
